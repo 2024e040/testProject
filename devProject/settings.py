137ccd9978e2e682b3cd7f43dcfd028c6f34d77d
2024e040 <2024e040@hokuriku-u.ac.jp>
2026-01-12 23:19:18 +0900

    Remove debug_toolbar for Render

--- devProject/settings.py
+++ devProject/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     "testapp",
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +51,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = "devProject.urls"
@@ -127,6 +125,3 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
